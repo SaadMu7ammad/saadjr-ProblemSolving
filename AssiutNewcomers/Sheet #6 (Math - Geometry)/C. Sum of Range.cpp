02a38b710cb440793a1accd0336724e9178a5f11
SaadMu7ammad <74321306+SaadMu7ammad@users.noreply.github.com>
2022-11-05 09:54:37 +0200

    Create C. Sum of Range.cpp

---
+++ AssiutNewcomers/Sheet #6 (Math - Geometry)/C. Sum of Range.cpp
@@ -0,0 +1,125 @@
+//                     "اللــهــم ارزقنـــا الهـــدى و الســـداد"
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+#define ll long long
+#define loop(start, n) for (ll i = start; i < n; i++)
+#define lup(i,start,end) for (int i=(start) ; i<(end) ; i++)
+#define ld(i,start,end) for (int i=(start) ; i>=(end) ; i--)
+//#define endl "\n"
+#define sonic ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+long long gcd(long long a, long long b)
+{
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+long long lcm(long long a, long long b)
+{
+    return a / gcd(a, b) * b;
+}
+unsigned long long nCr(int n, int r) {
+    if (r > n)
+        return 0;
+    r = max(r, n - r); //nCr(n,r) = nCr(n,n-r)
+    unsigned long long ans = 1, div = 1, i = r + 1;
+    while (i <= n) {
+        ans *= i;
+        i++;
+        ans /= div;
+        div++;
+    }
+    return ans;
+}
+unsigned long long nPr(int n, int r) {
+    if (r > n)
+        return 0;
+    unsigned long long p = 1, i = n - r + 1;
+    while (i <= n)
+        p *= i++;
+    return p;
+}
+
+bool sortbysec(pair< long long, long long> a, pair< long long, long long> b)
+{
+    if (a.second == b.second)
+    {
+        return (a.first > b.first);
+    }
+    return a.second > b.second;
+}
+int fibonacii(int indx) {
+    if (indx == 1 || indx == 2)return 1;
+    return fibonacii(indx - 1) + fibonacii(indx - 2);
+}
+unsigned long long factorial(unsigned long long num) {
+
+    if (num == 1 || num == 0)return 1;
+
+    return num * factorial(num - 1);
+}
+void primefactors(long long num) {
+    //Math_wk6_assiut
+    if (num == 1 || num == 0)return;
+    lup(i, 2, (i * i) + 1) {
+        while (num % i == 0) {
+            cout << i << " ";
+            num /= i;
+        }
+    }
+}
+int fib(int n) {
+    if (n == 1)return 0;
+    if (n == 2)return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+//int dx[8] = { 0,0,1,-1,1,-1,1,-1 };
+//int dy[8] = { 1,-1,0,0,1,-1,-1,1 };
+//int dx[4] = { 0,0,1,-1};
+//int dy[4] = { 1,-1,0,0 };
+int dx[4] = { 0,1 };//right and down only
+int dy[4] = { 1,0 };
+int visited[110][110];
+bool ok(int x, int y, int n) { return x >= 0 && y >= 0 && x < n&& y < n; }//check is in or not
+
+bool isprime(ll num) {
+    if (num == 1)return false;
+
+    for (long long i = 2; i * i <= num; i++) {
+        if (num % i == 0)return false;
+    }
+    return true;
+}
+int main()
+{
+    sonic;
+    ll a, b;
+    cin >> a >> b;
+    if (b < a) {//make A is a start always so it must be less than B
+        swap(a, b);
+    }
+   // cout << a << b << "\n";
+    ll n = b - a + 1;//no of numbers 
+    cout << ((a + b)* (n))/2<<"\n";
+    //to calc the even sequence
+    ll start, end;
+
+    if (a % 2 == 0)
+        start = a;
+    else
+        start = a + 1;
+
+    if (b % 2 == 0)
+        end = b;
+    else
+        end = b - 1;
+  //  cout << "st" << start << " " << end << "\n";
+    //to cal the numbers between 2even's interval 
+    //div'em directly
+    //the above if is for preprocessing the condition
+    cout << ((start +end)/2) *((end-start)/2+1) << "\n";
+
+    cout << ((a + b) * (n)) / 2 - ((start + end) / 2) * ((end - start) / 2 + 1) << "\n";//tot-even=odd
+    //cerr << "Time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+}
